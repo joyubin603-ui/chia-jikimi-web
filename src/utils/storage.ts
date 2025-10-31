92ac079ed0309753151735ca5effed84fdf8c72b
joyubin603-ui <joyubin603@gmail.com>
2025-10-31 14:00:34 +0900

    Update storage.ts

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,1 +1,13 @@
+import localforage from 'localforage'
 
+localforage.config({ name: 'chia-jikimi' })
+
+export const getPoints = async (): Promise<number> => {
+  const points = await localforage.getItem<number>('points')
+  return points || 0
+}
+
+export const addPoints = async (amount: number) => {
+  const current = await getPoints()
+  await localforage.setItem('points', current + amount)
+}
